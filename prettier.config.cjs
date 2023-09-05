fb73d2d9cc01a7f5dcd6ecedb96e3ecc85f88d99
@websnacks.com <preetam.slot@gmail.com>
2023-09-05 09:38:31 +0200

    template tweaks

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
