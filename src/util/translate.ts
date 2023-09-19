31aff1bc46832a7a81d0cb034cb7486d1258b0d0
@websnacks.com <preetam.slot@gmail.com>
2023-09-19 17:52:07 +0200

    add language hr

--- src/util/translate.ts
+++ src/util/translate.ts
@@ -6,13 +6,15 @@ import nl from '@locales/nl.json';
 import en from '@locales/en.json';
 import de from '@locales/de.json';
 import es from '@locales/es.json';
+import hr from '@locales/hr.json';
 const lang = import.meta.env.WEBSITE_LANGUAGE;
 export const t = (field: string): string => {
     const translations: Record<string, Locale>  = {
         en: en,
         nl: nl,
         es: es,
-        de: de
+        de: de,
+        hr: hr,
     };
 
     if (translations[lang] && translations[lang][field]) {
