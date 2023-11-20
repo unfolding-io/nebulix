224cc089e27ed21f637931fa5d7eac34332ad057
@websnacks.com <preetam.slot@gmail.com>
2023-11-20 08:51:43 +0100

    add color switch + upgrade
    @astrojs/check                ^0.2.1  →   ^0.3.1
     @astrojs/mdx                  ^1.1.2  →   ^1.1.5
     @astrojs/sitemap              ^3.0.2  →   ^3.0.3
     @astrojs/vue                  ^3.0.2  →   ^3.0.4
     @vueuse/components           ^10.5.0  →  ^10.6.1
     @vueuse/integrations         ^10.5.0  →  ^10.6.1
     astro                         ^3.3.3  →   ^3.5.5
     astro-compress                ^2.1.5  →   ^2.2.3
     convert                      ^4.13.2  →  ^4.14.0
     marked                        ^9.1.2  →  ^10.0.0
     nanostores                    ^0.9.4  →   ^0.9.5
     pagefind                      ^1.0.3  →   ^1.0.4
     prettier                      ^3.0.3  →   ^3.1.0
     prettier-plugin-astro        ^0.12.0  →  ^0.12.2
     prettier-plugin-tailwindcss   ^0.5.6  →   ^0.5.7
     tailwindcss                   ^3.3.3  →   ^3.3.5
     vue                           ^3.3.6  →   ^3.3.8

---
+++ src/components/common/ColorSwitch.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    @click="toggleDark()"
+    aria-label="Toggle color scheme"
+    class="grid h-full place-items-center"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Zm0-1.5v-17a8.5 8.5 0 0 1 0 17Z"
+      />
+    </svg>
+  </button>
+</template>
+
+<script setup>
+const toggleDark = () => {
+  const theme =
+    localStorage.getItem("color-scheme") === "dark" ? "light" : "dark";
+
+  localStorage.setItem("color-scheme", theme);
+  document.querySelector("html").dataset.theme = theme;
+};
+</script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 module.exports = {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
