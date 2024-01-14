71950f84fb314e3e67b748a8e31faac72db15832
@websnacks.com <preetam.slot@gmail.com>
2024-01-14 18:46:34 +0100

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,13 @@ import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from "rehype-external-links";
 import fauxRemarkEmbedder from "@remark-embedder/core";
 import fauxOembedTransformer from "@remark-embedder/transformer-oembed";
+
 const remarkEmbedder = fauxRemarkEmbedder.default;
 const oembedTransformer = fauxOembedTransformer.default;
+
 import vue from "@astrojs/vue";
 /** @type {import('astro-m2dx').Options} */
-import prefetch from "@astrojs/prefetch";
-import compress from "astro-compress";
+
 const m2dxOptions = {
   exportComponents: true,
   unwrapImages: true,
@@ -32,14 +33,6 @@ export default defineConfig({
       appEntrypoint: "/src/pages/_app",
     }),
     astroImageTools,
-    prefetch(),
-    compress({
-      CSS: true,
-      HTML: false,
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-    }),
   ],
   markdown: {
     extendDefaultPlugins: true,
@@ -78,4 +71,7 @@ export default defineConfig({
     inlineStylesheets: "always",
   },
   scopedStyleStrategy: "attribute",
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
 });
